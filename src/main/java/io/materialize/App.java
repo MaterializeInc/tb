389c61238e7c87a9b8ab7fa3d865067ee68817e1
Arjun Narayan <arjun@materialize.io>
2019-11-22 11:11:19 -0500

    initial maven initialization

---
+++ src/main/java/io/materialize/App.java
@@ -0,0 +1,13 @@
+package io.materialize;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
